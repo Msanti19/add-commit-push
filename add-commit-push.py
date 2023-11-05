4a863e709d617d627753b61508774b1141a5d1e1
Msanti19 <143824051+Msanti19@users.noreply.github.com>
2023-11-05 13:27:08 -0600

    Add files via upload

---
+++ add-commit-push.py
@@ -0,0 +1,38 @@
+import os
+import sys
+
+print("testing")
+numOfArgs = len(sys.argv)
+print("Total Arguments Passed: ", numOfArgs)
+message = "Update files"
+if numOfArgs == 3:
+    if sys.argv[1] == "-m":
+        print ("Number of args = 3 and p2 -m")
+        message = sys.argv[2]
+
+print (message)        
+
+
+
+print ("Add Commit Push")
+print("\ngit status")
+os.syste,("git status")
+
+
+if numOfArgs != 2:    
+    print("Do you want to continue with add commit push? (y):")
+    confirm = input()
+    if confirm != "y":
+        print("Canceling ", confirm)
+        quit()    
+
+print("\ngit add -A")
+os.system("\ngit add -A")
+
+commitStatement = '\ngit commit -m "' + message + '"'
+print(commitStatement)
+os.system(commitStatement)
+
+
+print("\ngit push")
+os.system("git push")
